9f08c3ea07d35795f2859f0b63193607818f07a0
boriskulakov <>
2023-08-28 22:23:32 +0500

    подготовил проект
    добавил typescript и библиотеку classnames
    загрузил шрифты и картинки

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './css/App.css'
+
+function App() {
+  return (
+    <div className="page">
+      <h1 className="title">
+        Regexp Constructor
+        <a
+          href="https://github.com/boriskulakov/regexp-constructor"
+          target="blank"
+          className="git_link"
+        ></a>
+      </h1>
+      <div className="main"></div>
+      <div id="portal"></div>
+    </div>
+  )
+}
+
+export default App
